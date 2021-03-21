a188db46212f0939fa9fa263775e95d0a1fee857
djablonski2 <djablonski2@gmail.com>
2021-03-21 17:51:36 +0100

    dodanie komentarzy

--- src/com/djablo/pakiet/Cogwheel.java
+++ src/com/djablo/pakiet/Cogwheel.java
@@ -1,14 +1,16 @@
 package com.djablo.pakiet;
 
 public class Cogwheel {
-    private int size;
-    private int numberOfCogs;
+    private int size; //atrybut klasy
+    private int numberOfCogs; //atrybut klasy
 
-    public Cogwheel() {
+    public Cogwheel() { //konstruktor bo nie ma typu zwracanego
     }
 
-    public Cogwheel(int size, int noCogs) {
-        this.size = size;
+    public Cogwheel(int size, int noCogs) { //konstruktor bo nie ma typu zwracanego
+        this.size = size; //zawiera dodatkowe słowo kluczowe this aby odróżnić parametr size od atrybutu size
         numberOfCogs = noCogs;
     }
+
+
 }
--- src/com/djablo/pakiet/Engine.java
+++ src/com/djablo/pakiet/Engine.java
@@ -3,14 +3,14 @@ package com.djablo.pakiet;
 import com.djablo.pakiet.Cogwheel;
 
 public class Engine {
-    private boolean started;
-    private Cogwheel cogwheel;
+    private boolean started; //atrybut klasy
+    private Cogwheel cogwheel; //atrybut klasy; typem może być też inna klasa
 
-    public Engine() {
-        cogwheel = new Cogwheel(4, 450);
+    public Engine() { //konstruktor bo nie ma typu zwracanego
+        cogwheel = new Cogwheel(4, 450); //wywołanie konstruktora, inaczej - tworzenie instancji klasy
         started = true;
     }
-    public Engine(Cogwheel cogwheel) {
+    public Engine(Cogwheel cogwheel) { //konstruktor bo nie ma typu zwracanego
         this.cogwheel = cogwheel;
     }
 
@@ -29,4 +29,6 @@ public class Engine {
     public void setStarted(boolean started) {
         this.started = started;
     }
+
+
 }
